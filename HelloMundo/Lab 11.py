6daee095ee6258726db7d8a805dd416f23a822a7
MarieCou <mariecoutart@gmail.com>
2024-05-24 14:19:00 +0200

    Lab 11

--- HelloMundo/Lab 11.py
+++ HelloMundo/Lab 11.py
@@ -1,6 +1,37 @@
+# Read file
+
+# Program to read the specific length 
+# of characters in a file using read() function
+#file = open("preproinsulin-seq.txt", "r")
 
 
-Python 3.6.12 (default, Aug 31 2020, 18:56:18)
-[GCC 4.8.5 20150623 (Red Hat 4.8.5-28)] on linux
-Type "help", "copyright", "credits" or "license" for more information.
->>>
+
+
+
+
+# Python3.6  
+# Coding: utf-8  
+# Store the human preproinsulin sequence in a variable called preproinsulin:  
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"  
+# Store the remaining sequence elements of human insulin in variables:  
+lsInsulin = "malwmrllpllallalwgpdpaaa"  
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"  
+aInsulin = "giveqcctsicslyqlenycn"  
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"  
+insulin = bInsulin + aInsulin
+
+pKR = {'y':10.07,'c': 8.18,'k':10.53,'h':6.00,'r':12.48,'d':3.65,'e':4.25}
+
+seqCount = ({x: float(insulin.count(x)) for x in ['y','c','k','h','r','d','e']})
+
+pH = 0
+while(pH <= 14):
+
+  netCharge = (
+    +(sum({x: ((seqCount[x]*(10**pKR[x]))/((10**pH)+(10**pKR[x]))) \
+    for x in ['k','h','r']}.values()))
+    -(sum({x: ((seqCount[x]*(10**pH))/((10**pH)+(10**pKR[x]))) \
+    for x in ['y','c','d','e']}.values())))
+  print('{0:.2f}'.format(pH), netCharge)
+  pH +=1
+  
