4d271b9122f90bfd1bfca7d6407f4f1c16f12143
MarieCou <mariecoutart@gmail.com>
2024-05-24 14:13:10 +0200

    Lab 11

--- HelloMundo/Lab 11.py
+++ HelloMundo/Lab 11.py
@@ -1,4 +1,6 @@
 
-print("Hello, World")
-print("Hello, World")
 
+Python 3.6.12 (default, Aug 31 2020, 18:56:18)
+[GCC 4.8.5 20150623 (Red Hat 4.8.5-28)] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>>
