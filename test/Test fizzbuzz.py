e242f519a490504de6689b201d33e0dd539818dc
MarieCou <mariecoutart@gmail.com>
2024-05-27 15:02:50 +0200

    ¨debugger.py¨¨

---
+++ test/Test fizzbuzz.py
@@ -0,0 +1,8 @@
+Si el numero es divisible por 3, se devuelve "Fizz"
+# - Si el numero es divisible por 5, se devuelve "Buzz"
+# - Si el numero es divisible por 3 y 5, se devuelve "FizzBuzz"
+# - En cualquier otro caso, se devuelve el numero
+def fizzbuzz (numero):
+    if numero % == 0 and numero % 5 ==0:
+        return ¨Fizzbuzz¨
+    
